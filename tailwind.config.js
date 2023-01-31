1af731c90687c95a8f44119bd0c5708bd9a7ba7e
Ankush Dutta <ankush.ad050203@gmail.com>
2023-01-31 21:00:20 +0530

    custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors : {
+      'green1' : '#7DCE13',
+      'green2' : '#3EC70B',
+      'green3' : '#2B7A0B',
+      'yellow1' : '#EAE509',
+      'white1' : '#FFFFFF',
+      'grey1' : '#e5e7eb',
+      'black1' : '#000000',
+      'white2':'#F5F5F5',
+    },
+    fontFamily:{
+      'poppins':['Poppins']
+    }
   },
   plugins: [],
 }
